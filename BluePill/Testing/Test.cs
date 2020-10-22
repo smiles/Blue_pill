904b92de5e918bd11cbd7e18559d664340f293ec
Smiles <vissa7@gmail.com>
2020-10-22 14:01:12 -0400

    base line testing ready for CI testing

---
+++ BluePill/Testing/Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BluePill.Testing
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class Test : ControllerBase
+    {
+        public string Get()
+        {
+            return "Test successful";
+        }
+    }
+}
