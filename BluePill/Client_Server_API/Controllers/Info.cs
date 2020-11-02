14f2a9d765271ecc5a3a9aabb4f5c991bf4f66dd
unknown <vissa7@gmail.com>
2020-11-01 21:14:04 -0500

    first controller for versioning for the client-server done

---
+++ BluePill/Client_Server_API/Controllers/Info.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BluePill.Client_Server_API.JSON;
+
+namespace BluePill.Client_Server_API.Controllers
+{
+    [ApiController]
+    [Produces("application/json")]
+    public class Info : ControllerBase
+    {
+        private readonly JSON.Version ver = new JSON.Version();
+
+        [HttpGet]
+        [Route("/_matrix/client/versions")]
+        public IActionResult Ver()
+        {
+            return Ok(ver);
+        }
+        
+    }
+}
---
+++ BluePill/Client_Server_API/JSON/Version.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BluePill.Client_Server_API.JSON
+{
+    public class Version
+    {
+        public string[] Versions { get; } = { "r.0.6.0", "r.0.6.1" };
+    }
+}
